cdf87ddec1359ea633ec12c938287a885af2ff24
alaminhossen899 <alaminhossen899@gmail.com>
2021-01-04 23:31:33 +0600

    install vue js in configure

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
-<div class="wrapper">
+<div class="wrapper" id="app">
     <!-- Navbar -->
     @include('Backend.include.navbar')
     <!-- /navbar -->
