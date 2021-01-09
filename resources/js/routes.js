11cb9a112564feee1a0e0d053a5e34b39b00d8d4
alaminhossen899 <alaminhossen899@gmail.com>
2021-01-09 22:06:38 +0600

    comment

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,6 @@
 import adminDashboard from './components/admin/dashboard.vue'
 import adminMaster from './components/admin/AdminMaster.vue'
 export const routes = [
-    { path: '/admin-dashboard', component: 'adminDashboard' },
+    { path: '/admin-dashboard', component: 'adminDashboard' }, //dashboard
     { path: '/admin-master', component: 'adminMaster' },
 ]
