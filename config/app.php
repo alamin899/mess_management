34812c74be50005087950c30f70c763181c3c7b4
alaminhossen899 <alaminhossen899@gmail.com>
2021-01-17 21:42:58 +0600

    image intervention package installed

--- config/app.php
+++ config/app.php
@@ -165,7 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Intervention\Image\ImageServiceProvider::class,
+        
         /*
          * Application Service Providers...
          */
@@ -227,6 +228,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+
+
 
     ],
 
