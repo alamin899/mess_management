3c66a90fb13227b83ee4d8d897be3df20ce284db
alaminhossen899 <alaminhossen899@gmail.com>
2021-01-24 11:21:47 +0600

    all necessary file created for payment head crud

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\PaymentHead;
+use Database\Seeders\PaymentHeadSeeder;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        PaymentHead::observe(PaymentHeadSeeder::class);
     }
 }
