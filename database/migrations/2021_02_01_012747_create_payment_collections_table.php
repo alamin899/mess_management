2f54d4c339970a1da73a951f37c919b95eda93c6
alaminhossen899 <alaminhossen899@gmail.com>
2021-02-01 07:37:37 +0600

    payment collection model and migration created

---
+++ database/migrations/2021_02_01_012747_create_payment_collections_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\PaymentCollection;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentCollectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create((new PaymentCollection())->getTable(), function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('head_id');
+            $table->date('payment_date');
+            $table->float('amount');
+            $table->string('remarks');
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->integer('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists((new PaymentCollection())->getTable());
+    }
+}
