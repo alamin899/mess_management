ad70ea5bab578551ac2bd699c5ee576e44eddc31
alaminhossen899 <alaminhossen899@gmail.com>
2021-02-04 01:51:30 +0600

    multiselect with group data

--- resources/js/components/payment_schedule/index.vue
+++ resources/js/components/payment_schedule/index.vue
@@ -1,19 +1,13 @@
 <template>
     <div>
-        <label class="typo__label">Simple select / dropdown</label>
-        <multiselect v-model="value"
-                     :options="options"
-                     :multiple="true"
-                     :close-on-select="false"
-                     :clear-on-select="true"
-                     :preserve-search="true"
-                     placeholder="Pick some" label="name" track-by="name"
-                     :preselect-first="true">
-            <template slot="selection"
-                      slot-scope="{ values, search, isOpen }"
-            ><span class="multiselect__single" v-if="values.length &amp;&amp; !isOpen">{{ values.length }} options selected</span></template>
-        </multiselect>
-<!--        <multiselect v-model="value" :options="options" :multiple="true" group-values="libs" group-label="language" :group-select="true" placeholder="Type to search" track-by="name" label="name"><span slot="noResult">Oops! No elements found. Consider changing the search query.</span></multiselect>-->
+        <label class="typo__label">Groups</label>
+        <multiselect
+                v-model="value"
+                :options="options"
+                :multiple="true"
+                group-values="libs"
+                group-label="language"
+                :group-select="true" :close-on-select="false" placeholder="Type to search" track-by="name" label="name"><span slot="noResult">Oops! No elements found. Consider changing the search query.</span></multiselect>
         <pre class="language-json"><code>{{ value  }}</code></pre>
     </div>
 </template>
@@ -23,196 +17,20 @@
         name: "index",
         data () {
             return {
-                value: [],
                 options: [
-                    { name: '', language: '' },
-                    { name: 'Vue.js', language: 'JavaScript' },
-                    { name: 'Adonis', language: 'JavaScript' },
-                    { name: 'Rails', language: 'Ruby' },
-                    { name: 'Sinatra', language: 'Ruby' },
-                    { name: 'Laravel', language: 'PHP' },
-                    { name: 'Phoenix', language: 'Elixir' }
-                ]
+                    {
+                        language: 'Javascript',
+                        libs: [
+                            { name: 'Vue.js', category: 'Front-end' },
+                            { name: 'Adonis', category: 'Backend' },
+                            { name: 'Rails', category: 'Backend' },
+                            { name: 'Sinatra', category: 'Backend' },
+                            { name: 'Laravel', category: 'Backend' }]
+                    },
+                ],
+                value: ''
             }
         }
-        // data () {
-        //     return {
-        //         options: [
-        //             {
-        //                 language: 'Javascript',
-        //                 libs: [
-        //                     { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' }, { name: 'Vue.js', category: 'Front-end' },
-        //                     { name: 'Adonis', category: 'Backend' },
-        //                     { name: 'Rails', category: 'Backend' },
-        //                     { name: 'Sinatra', category: 'Backend' },
-        //                     { name: 'Laravel', category: 'Backend' },
-        //                     { name: 'Phoenix', category: 'Backend' },
-        //                 ]
-        //             },
-        //         ],
-        //         value: []
-        //     }
-        // }
     }
 </script>
 
