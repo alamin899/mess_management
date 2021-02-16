c621479fee5656af742e4e56280deef549579b9e
alaminhossen899 <alaminhossen899@gmail.com>
2021-02-16 23:18:58 +0600

    meal migration and reltion create

--- database/migrations/2021_02_16_170559_create_meals_table.php
+++ database/migrations/2021_02_16_170559_create_meals_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Meal;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,9 +14,20 @@ class CreateMealsTable extends Migration
      */
     public function up()
     {
-        Schema::create('meals', function (Blueprint $table) {
+        Schema::create((new Meal())->getTable(), function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->date('date');
+            $table->float('breakfast')->nullable();
+            $table->float('lunch')->nullable();
+            $table->float('dinner')->nullable();
+            $table->float('total_meal');
+            $table->string('remarks');
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->integer('deleted_by')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -26,6 +38,6 @@ class CreateMealsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('meals');
+        Schema::dropIfExists((new Meal())->getTable());
     }
 }
