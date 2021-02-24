80e672074d892bf11dc8d8097d169f080ecf3a93
alaminhossen899 <alaminhossen899@gmail.com>
2021-02-24 20:31:03 +0600

    show date in form

--- app/Http/Traits/DateTimeTrait.php
+++ app/Http/Traits/DateTimeTrait.php
@@ -6,5 +6,19 @@ namespace App\Http\Traits;
 
 trait DateTimeTrait
 {
+    public function showDate($date = null, $format = "d-M-Y")
+    {
+        if ($date) {
+            return date($format, strtotime($date));
+        }
+        return date($format);
+    }
 
+    public function showDateInForm($date = null, $format = "Y-m-d")
+    {
+        if ($date) {
+            return date($format, strtotime($date));
+        }
+        return date($format);
+    }
 }
