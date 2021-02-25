4cd4df0532552d9fa1ed61a8c440184795f0457a
alaminhossen899 <alaminhossen899@gmail.com>
2021-02-25 23:35:30 +0600

    all meals query

--- app/Http/Repositories/MealRepository.php
+++ app/Http/Repositories/MealRepository.php
@@ -4,7 +4,20 @@
 namespace App\Http\Repositories;
 
 
+use App\Models\Meal;
+
 class MealRepository
 {
+    public function getMeals($user_id = '', $date = '', $withTrashed = false)
+    {
+        ($withTrashed)? $meals = Meal::withTrashed()->latest(): $meals = Meal::query();
+        $meals->when((!empty($user_id)), function ($meals) use ($user_id) {
+            $meals->where('user_id', $user_id);
+        });
+        $meals->when((!empty($date)), function ($meals) use ($date) {
+            $meals->where('date', $date);
+        });
+        return $meals;
 
+    }
 }
