b15acd2a3ca2e841d64eaa156aa18aafecd3cc47
alaminhossen899 <alaminhossen899@gmail.com>
2021-02-26 11:42:04 +0600

    meal index data using repo

--- app/Http/Controllers/Api/Backend/MealController.php
+++ app/Http/Controllers/Api/Backend/MealController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Http\Repositories\MealRepository;
+use App\Http\Resources\MealResource;
 use Illuminate\Http\Request;
 
 class MealController extends Controller
@@ -11,5 +12,10 @@ class MealController extends Controller
     public function __construct(MealRepository $mealRepository)
     {
         $this->mealRepository = $mealRepository;
+    }
+
+    public function index(Request $request)
+    {
+        return ($request->ajax()) ? MealResource::collection($this->mealRepository->getData(true,true)) : abort(401 , 'Bad request');
     }
 }
