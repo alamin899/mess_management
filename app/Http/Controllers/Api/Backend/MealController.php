2f2675ce05c1acf50f1f30808e1e2ed5c2216079
alaminhossen899 <alaminhossen899@gmail.com>
2021-02-26 11:41:30 +0600

    meal repository initite using constructor

--- app/Http/Controllers/Api/Backend/MealController.php
+++ app/Http/Controllers/Api/Backend/MealController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Repositories\MealRepository;
 use Illuminate\Http\Request;
 
 class MealController extends Controller
 {
-    //
+    public function __construct(MealRepository $mealRepository)
+    {
+        $this->mealRepository = $mealRepository;
+    }
 }
