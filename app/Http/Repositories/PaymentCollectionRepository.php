55103c79d998547d8bb861779ce8d2f7462fe58c
alaminhossen899 <alaminhossen899@gmail.com>
2021-02-28 18:34:44 +0600

    payment collection getcollection functionality

--- app/Http/Repositories/PaymentCollectionRepository.php
+++ app/Http/Repositories/PaymentCollectionRepository.php
@@ -5,6 +5,7 @@ namespace App\Http\Repositories;
 
 
 use App\Http\Traits\Custom;
+use App\Models\PaymentCollection;
 
 class PaymentCollectionRepository
 {
@@ -14,8 +15,18 @@ class PaymentCollectionRepository
         return ($paginate)? $this->getPaymentCollections('','','',$status,'', $withTrashed )
             ->paginate($this->getPaginate()) : $this->getPaymentCollections('','','',$status,'', $withTrashed)->get();
     }
-    public function getPaymentCollections()
+    public function getPaymentCollections($user_id = '', $head_id = '', $status = '',  $withTrashed = false)
     {
-        
+        ($withTrashed) ? $paymentCollections = PaymentCollection::withTrashed()->latest() : $paymentCollections = PaymentCollection::query()->latest();
+        $paymentCollections->when((!empty($user_id)), function ($paymentCollections) use ($user_id) {
+            $paymentCollections->where('user_id', $user_id);
+        });
+        $paymentCollections->when((!empty($head_id)), function ($paymentCollections) use ($head_id) {
+            $paymentCollections->where('head_id', $head_id);
+        });
+        $paymentCollections->when((!empty($status)), function ($paymentCollections) use ($status) {
+            $paymentCollections->where('status', $status);
+        });
+        return $paymentCollections;
     }
 }
