4e22bfe56f5a43781f39b64c318b16c3c73ccd7b
alaminhossen899 <alaminhossen899@gmail.com>
2021-03-02 15:18:41 +0600

    payment collection repo initiate in construst

--- app/Http/Controllers/Api/Backend/PaymentCollectionController.php
+++ app/Http/Controllers/Api/Backend/PaymentCollectionController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Repositories\PaymentCollectionRepository;
 use Illuminate\Http\Request;
 
 class PaymentCollectionController extends Controller
 {
-    //
+    public function __construct(PaymentCollectionRepository $paymentCollectionRepository)
+    {
+        $this->paymentCollectionRepository = $paymentCollectionRepository;
+    }
 }
