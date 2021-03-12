4e2bf44e4673d6c104cf98aadb2aab5622e5ee68
alaminhossen899 <alaminhossen899@gmail.com>
2021-03-12 20:29:35 +0600

    migration and model create

---
+++ database/migrations/2021_03_12_142307_create_bazars_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Bazar;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBazarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create((new Bazar())->getTable(), function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('payment_date');
+            $table->float('amount');
+            $table->string('remarks');
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->integer('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists((new Bazar())->getTable());
+    }
+}
