d817d40c75d23cacb607bb10e6d8c73b042b090e
alaminhossen899 <alaminhossen899@gmail.com>
2021-03-24 08:38:34 +0600

    meal create label

--- resources/js/components/meal/create.vue
+++ resources/js/components/meal/create.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-
+  <div class="container">
+    <br>
+    <div class="row">
+      <div class="col-md-12 col-sm-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Meal Create</h3>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
